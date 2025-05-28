54579ecff6d32e17f632478475b9e5eaa069fb62
Annamalikalugun <annamalikalugun@tripledotss.com>
2025-05-28 13:00:53 +0530

    file updated
    file updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/HomePage';
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
-  );
+import React, { Component, Fragment } from 'react';
+import AppRoute from './routes/AppRoute';
+import { HashRouter } from 'react-router-dom';
+
+export class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <HashRouter>
+          <AppRoute />
+        </HashRouter>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
